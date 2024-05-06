650e72a9b21fd0246ecf7c6fc380d404f80bcc29
soyel <soyeloygarciamartinez@gmail.com>
2024-05-06 13:51:06 +0200

    cambios929210

--- Flexbox/main.js
+++ Flexbox/main.js
@@ -5,3 +5,22 @@ btnSwitch.addEventListener('click', () => {
     btnSwitch.classList.toggle('active');
 });
 
+$(document).ready(main);
+
+var contador = 0;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+}
